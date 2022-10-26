9e223da815ea6385003fcef76d54aeb1b69844ab
Anees Ahmad <anees.ahmad@ibm.com>
2022-10-26 12:11:01 -0700

    redeploy

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+
 passport.serializeUser((user, done) => {
   done(null,user.id);
 });
