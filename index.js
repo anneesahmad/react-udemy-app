a552f94c7edbd7c2f76ba3392a421a91e6c64b0e
Anees Ahmad <anees.ahmad@ibm.com>
2022-11-24 01:41:09 +0500

    added and client

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('react-app/build'));
   const path = require('path');
   app.get('*',(req,res) => {
     res.sendFile(path.resolve(__dirname, 'react-app','build','index.html'));
